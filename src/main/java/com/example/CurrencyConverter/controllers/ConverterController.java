e58f4bf30f48988e3417f24c48b5d00f8f29dc84
vaivavai <vaiva.vaitkeviciute@gmail.com>
2023-01-08 16:15:52 +0200

    Added @CrossOrigin to communicate with frontend

--- src/main/java/com/example/CurrencyConverter/controllers/ConverterController.java
+++ src/main/java/com/example/CurrencyConverter/controllers/ConverterController.java
@@ -2,12 +2,14 @@ package com.example.CurrencyConverter.controllers;
 
 import com.example.CurrencyConverter.services.ConverterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping(path = "api/v1/converter")
 public class ConverterController {
 
